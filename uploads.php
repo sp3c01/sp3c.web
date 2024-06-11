5317f341f0befc11268aa7abdde89ac20f8b190e
sp3c01 <163803149+sp3c01@users.noreply.github.com>
2024-06-11 16:35:47 -0300

    Update uploads.php

--- uploads.php
+++ uploads.php
@@ -1,24 +1,18 @@
 <?php
-// Verifica se o arquivo foi enviado corretamente
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
-        // Diretório onde os arquivos serão armazenados
+        
         $uploadDir = 'uploads/';
         
-        // Nome do arquivo enviado
         $fileName = basename($_FILES['file']['name']);
         
-        // Caminho completo do arquivo no servidor
         $filePath = $uploadDir . $fileName;
         
-        // Move o arquivo do diretório temporário para o diretório de uploads
         if (move_uploaded_file($_FILES['file']['tmp_name'], $filePath)) {
             echo "<p>Arquivo enviado com sucesso.</p>";
 
-            // Lê o conteúdo do arquivo
             $fileContent = file_get_contents($filePath);
-
-            // Atualiza o conteúdo da página principal com o conteúdo do arquivo enviado
+            
             echo "<script>";
             echo "document.addEventListener('DOMContentLoaded', function() {";
             echo "    fetch('index.html', { method: 'GET' })";

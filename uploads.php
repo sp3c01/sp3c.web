b670d4e67a89ac392d0472c4d96bea6393fd395e
sp3c01 <163803149+sp3c01@users.noreply.github.com>
2024-06-11 16:45:05 -0300

    Update uploads.php

--- uploads.php
+++ uploads.php
@@ -1,35 +1,19 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
-        
-        $uploadDir = 'uploads/';
-        
-        $fileName = basename($_FILES['file']['name']);
-        
-        $filePath = $uploadDir . $fileName;
-        
-        if (move_uploaded_file($_FILES['file']['tmp_name'], $filePath)) {
-            echo "<p>Arquivo enviado com sucesso.</p>";
-
-            $fileContent = file_get_contents($filePath);
-            
-            echo "<script>";
-            echo "document.addEventListener('DOMContentLoaded', function() {";
-            echo "    fetch('index.html', { method: 'GET' })";
-            echo "    .then(response => response.text())";
-            echo "    .then(data => {";
-            echo "        document.body.innerHTML = data;";
-            echo "    })";
-            echo "    .catch(error => {";
-            echo "        console.error('Erro ao carregar a página principal:', error);";
-            echo "    });";
-            echo "});";
-            echo "</script>";
-        } else {
-            echo "<p>Erro ao enviar o arquivo.</p>";
-        }
-    } else {
-        echo "<p>Erro ao processar o upload do arquivo.</p>";
-    }
-}
-?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+<head>
+<title>index /admin/uploads</title>
+</head>
+<body>
+<h1>Index of /admin/uploads</h1>
+<table>
+<tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>
+<tr><th colspan="5"><hr></th></tr>
+<tr><td valign="top"><img src="/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/admin/">Parent Directory</a></td><td>&nbsp;</td><td align="right"> - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/image2.gif" alt="[IMG]"></td><td><a href="1490906279.jpg">1490906279.jpg</a></td><td align="right">2020-07-03 15:38 </td><td align="right">331K</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/image2.gif" alt="[IMG]"></td><td><a href="dsc_0699-min.jpg">dsc_0699-min.jpg</a></td><td align="right">2020-07-03 15:38 </td><td align="right">1.2M</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/image2.gif" alt="[IMG]"></td><td><a href="north-korea-science-technology.jpg">north-korea-science-technology.jpg</a></td><td align="right">2020-07-03 15:38 </td><td align="right">421K</td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td><td><a href="shel.php5">shel.php5</a></td><td align="right">2024-06-11 16:14 </td><td align="right"> 41 </td><td>&nbsp;</td></tr>
+<tr><th colspan="5"><hr></th></tr>
+</table>
+<address>Apache/2.4.29 (Ubuntu) Server at www.bancocn.com Port 80</address>
+</body></html>

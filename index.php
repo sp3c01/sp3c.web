eb5b0636c5f8b142b311d9257c3bf452ba294809
sp3c01 <163803149+sp3c01@users.noreply.github.com>
2024-06-11 16:56:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<link rel="stylesheet" id="base" href="/css/default.css" type="text/css" media="screen" />
+<title>DarkNet</title>
+<body>
+<div id="header">
+<div id="logo">
+<h1><a href="index.php">DarkNet</a></h1>
+</div>
+<div id="menu">
+<ul>
+<li class="active">
+<a href="/"> Home |</a>
+</li>
+<li>
+<a href="/admin/">Manage pictures |</a>
+</li>
+<li>
+<a href="/admin/new.php">New picture |</a>
+</li>
+<li>
+<a href="/admin/logout.php">Logout</a>
+</li>
+</ul>
+</div>
+</div>
+<table border="1">
+<div>
+<tr><td><a href="../show.php?id=7">estatua</a></td><td><a href="del.php?id=7">delete</a><td></tr><tr><td><a href="../show.php?id=4">predios</a></td><td><a href="del.php?id=4">delete</a><td></tr><tr><td><a href="../show.php?id=6">foguete</a></td><td><a href="del.php?id=6">delete</a><td></tr></table>
+<a href="new.php"> Add a new picture</a>
+</div>
+</body>
+</html>

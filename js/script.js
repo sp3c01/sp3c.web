d078cfd79f3510c7fed72bee1c4c07cecc9ad25c
sp3c01 <meblackhatt1@gmail.com>
2024-09-07 16:45:00 -0300

    Updatemov

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const links = [
+    { name: 'Meu Malvado Favorito', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Fkdy90Znd6bjk5OENvMWdidmhjMzFLcEhiNzd0RllUUENCZFlmSXNOV210bE9YNjZQdEFjRmswMFViMm5iMnJneGorUkV4bzM3aWZwZ2NmYnNWUTN4a3IyKzJXdEJDb1Z5WHdJajg2Rzg0TGRTQWhTTFJSU05lVDFYM3dGWkJpQlVZMTljMUk9' },
+    { name: 'Meu Malvado Favorito 2', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Fkdy90Znd6bjk5OENvMWdidmhjMzFLcEhiNzN0RllUUENCZFlmSXNOV210bE9YNjZQdEFjRmswMFViMm5iMnJnd1RhUUd4cGQ0eVhUditlTXZuVmg0VEhSNGhlMkEwOFNnUVp2eC9xQjhhQzZHbkpQZUE5dmZzS1hSVGwwTTN5QlRMYzJFVE8rZkE9PQ==' },
+    { name: 'Meu Malvado Favorito 3', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Fkdy90Znd6bjk5OENvMWdidmhjMzFLcEhiN3p0RllUUENCZFlmSXNOV210bE9YNjZQdEFjRmswMFViMm5iMnJnekRlVUV4cHNxbVhGbGZ5OWcwOEMrbmpZMGppaUVod1RpeWw1dWRQeWtiTFRYSGRJTXo4d2Z1eVRmbU1LWng3d2I3OTljMUk9' },
+    { name: 'Meu Malvado Favorito 4', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Rkdy90Znd6bjk5OENvMWdidmhjMzFLcEhiN3Z0RllUUENCZFlmSXNOV210bE9YNjZQdEFjRmswMFViMm5iMnJnekRhWEVocHAvQTNKb3NpNHRFQXo3RFBKcEFLTGV6NE5xU2s1a2NpRm5LT05aU1lwT3h0WFk3SGhhbWdwZWdtMEJ1b2VlRE8rZkE9PQ==' },
+    { name: 'Jogador N° 1', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Fkdy90Znd6bjk5OENvMThadHdrdm9jSnJTYnY4Rm9ISVRUaDhMTWdFU0RzYkUwckRDSTlhYXc0OFZyNnNiMi9tMkRhRkVIVlcvQ2V3aSttVGxtc1M2WFgzOFFPSlowb2JxSGN0bFBDcHBvVzhjQ3A4SkExT0hPYVZRbXNxVWliN0J1c2M=' },
+    { name: 'Dragon Ball GT: O Legado de um Herói (Original)', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3eFRPbGN0MkVXR3ZkeGJxZnd6bDVaNGl6bElLdng0bTFxVktkTXJ5VnBtTEEwWkRPc0kyU2xrL1NIWEtTdlVKSzNaNFhidWlhR25pd0RTVUZROHB4aURoamZ2UDUwUTVzeldPL3llVkVnQWRtMzByait1N3FQWGFabkZoSERaZUV2clZRVnR4UURld0J1b2FKeWFvQ3pRPQ==' },
+    { name: 'Ted 2', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Nkdy90Znd6bjk5OENvMEVidDJsUC9ad3lCdUcyUzQ2Nlpob1VCN0ovVEdvck8weTBTSVZaYVFvL1ZycWphWEhsdGtUS1prVnY1ejNQZyt1NHVHa2MyVlR2NXlDT0V4SXNtUlFuek9xRDlLVFpaekJ4RDFrL0F0UDJOam9D' },
+    { name: 'Projeto X: Uma Festa Fora de Controle', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Fkdy90Znd6bjk5OENvMFVNdVE4dXlMbEFmOHVBVnBtTEEwWkRPc0kyU2xrL1NIWEtTdlVKSzNaNFhidWlhR25pd3pHUkZnY3B2MlMxaWRhKzhEUVR1MkhVcHdiUk5EZG9xU1UvbE1tbHZmU09SZ2RnY2hZK0NNamhYbnAzTGhlSlRwRU5aU1hK' },
+    { name: 'SuperBad - É Hoje', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Jkdy90Znd6bjk5OENvMFlPb1JrbDFhUk1GK0t6VE11VlFrcFhEcUF2TWtBZlFucnJEdmdhWVEwK1VyK2piR3JteGpPTkYwMVEzQnUybC9pb3VrNFE4WHZQNTJhc0RTb3VsU1krd2M2WW80T01Rd3B0SWdodk10dkJKVWd6VzJ2d1p3PT0=' },
+    { name: 'O Lobo de Wall Street', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Jkdy90Znd6bjk5OENvMW9Tc1JRbHg2QlZiZDJYVnBtTEEwWkRPc0kyU2xrL1NIWEtTdlVKSzNaNFhidWlhR25pd3pHWkd3SXAvUzN6c2ZHdzRrSmdyakQ1M1RLWE5qc050VDF6eE51Z29hVy9YbmQrRFFRamRNV2JkM2d1VXoyU1I1b2hNQ2FvQ3pRPQ==' },
+    { name: 'Divertida Mente', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Nkdy90Znd6bjk5OENvMUVJb1E4bDNhSlNGK0t6VE11VlFrcFhEcUF2TWtBZlFucnJEdmdhWVEwK1VyK2pibTdoeERHTmJnRnZ1alBWck5TNW9WUmo3Mm1IckRtSEVoSUxxRGt6bDg2cXNMS3NUbWNxQ1J0dUxjaktZMWswVVNhS0J1c2M=' },
+    { name: 'Divertida Mente 2', url: 'http://example.com/divertida-mente-2' },
+    { name: 'Até o Último Homem', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Rkdy90Znd6bjk5OENvMVFLdkE0dDJLRklGK0t6VE11VlFrcFhEcUF2TWtBZlFucnJEdmdhWVEwK1VyK2piR3ZseEQrTloxNUR3Z2ZDNitpQW1rQTEvR1dNN1R1TklWMXJxaUlBek5uK29veWlUamgzRHdseUk4NkdJVThuVGdtSkJ1b2FNek8rZkE9PQ==' },
+    { name: 'The Flash (2023)', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Fkdy90Znd6bjk5OENvMEVXdGgwdHc2UW9WUC8zUjVxT0JBTnNIcEoxUTB0dk5sLytNOE5XYlFzN1ZieWpiV3ZneFNyS1puUis2REMyaW95dWxtazh1R3FQd0FlcGRDc2dyUVVvZ2VTRXI0cWdYWE5WQVNScktMYlFhMTBQTTMySA==' },
+    { name: 'Um Lugar Silencioso: Dia Um', url: 'https://デ-ン-ッ-ク-ス-ン-デ-ド-ド-ル-ボ-ラ-ルグレフト-ブムフクトプドラア.ジ-1l1-グ.ッ-22-ク-11-ス-33-ル-99-プ-75-ゾ--エ--ブ--ス-ッ.ク.ス.ズ.ク.ジ.シグナルパブリコ-公共の識標-バレウドットグウクトットズヒ.セール/player3/download.api?download=NVNtelU0S0NBUWNDVkNNTzR4Rld3d0g0Z3M5bEJpT3Rkdy90Znd6bjk5OENvMEFUdnh3enc2Qkllc0NRUEwyNllqUURJb0Y0TkdZcFFrZklLTkZTRkg4NkoramlFQzNweERDU0Z3RXl0MmUvNnBPOHRtVUE1VnZWN0REY0VoVnZoQzE1djVuNWd2SzZjeVVxZnpwd052TE5mR29lVWhqeVd1ZzVMSG5VSFVMMnUrY0RSZz09' },
+    { name: '@كلمة المرور', url: 'https://www.xvideos.com/video.uaoivmh52bc/rebolei_e_sentei_gostoso_na_rola_do_safado_ele_nao_resistiu_e_gozou_gostoso#show-related' },
+    { name: 'كلمة المرور#', url: 'https://www.xvideos.com/video.ududhof226b/mais_uma_vez_nao_resisti_a_bunda_monumental_da_minha_enteada_-_fodendo_de_conchinha#show-related' },
+    { name: 'Divertida Mente', url: '' },
+    { name: 'Divertida Mente 2', url: 'http://example.com/divertida-mente-2' },
+    { name: 'Divertida Mente 2', url: 'http://example.com/divertida-mente-2' },
+    { name: 'Divertida Mente 2', url: 'http://example.com/divertida-mente-2' },
+    { name: 'Divertida Mente 2', url: 'http://example.com/divertida-mente-2' },
+    { name: 'Divertida Mente 2', url: 'http://example.com/divertida-mente-2' },
+    { name: 'Divertida Mente 2', url: 'http://example.com/divertida-mente-2' },
+];
+
+function search() {
+    const searchBox = document.getElementById('search-box');
+    const query = searchBox.value.toLowerCase();
+    const results = document.getElementById('results');
+    const infoBox = document.getElementById('info-box');
+
+    if (query.trim() === '') {
+        return false; 
+    }
+    
+    if (infoBox) {
+        infoBox.style.display = 'none';
+    }
+
+    results.innerHTML = '';
+
+    const uniqueLinks = new Set(
+        links.filter(link => link.name.toLowerCase().includes(query))
+    );
+
+    if (uniqueLinks.size > 0) {
+        uniqueLinks.forEach(link => {
+            const linkElement = document.createElement('div');
+            linkElement.className = 'link';
+            linkElement.innerHTML = `<a href="${link.url}" target="_blank">${link.name}</a>`;
+            results.appendChild(linkElement);
+        });
+    } else {
+        results.innerHTML = 'здесь ничего';
+    }
+
+    return false; 
+}

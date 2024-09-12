b8ea0ae48d7f7c5685c8adc023252d48bc05be37
sp3c01 <163803149+sp3c01@users.noreply.github.com>
2024-09-11 21:45:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('searchButton').addEventListener('click', function() {
     const query = document.getElementById('searchInput').value;
-    fetch(`https://project-name.glitch.me/search?query=${encodeURIComponent(query)}`)  // Substitua "project-name" pelo nome do seu projeto Glitch
+    fetch(`https://wood-linen-expansion.glitch.me/search?query=${encodeURIComponent(query)}`)  // Substitua "project-name" pelo nome do seu projeto Glitch
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);

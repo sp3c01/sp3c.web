4beb95bdfaae0a9ca2974b6ba9fa6c5095088dd5
user_dp <163803149+sp3c01@users.noreply.github.com>
2024-09-29 05:15:12 -0300

    Delete frontend directory

--- frontend/js/utils/getDateCurrent.js
+++
@@ -1,9 +0,0 @@
-const chatDate = document.querySelector(".chat-date");
-
-function getDateCurrent() {
-  const date = new Date().toLocaleDateString();
-
-  chatDate.textContent = date;
-}
-
-getDateCurrent();
